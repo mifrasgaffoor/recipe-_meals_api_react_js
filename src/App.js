4320570710d2f880a19f54e049712c7ccbda5feb
Mifras Gaffoor <81522853+mifrasgaffoor@users.noreply.github.com>
2021-08-28 12:27:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Cards from "./components/Card/Cards";
 import Footer from "./components/Footer/Footer";
-
-
 const App = () => {
-
- 
   return (
     <div className="main__container">
       <Navbar />
